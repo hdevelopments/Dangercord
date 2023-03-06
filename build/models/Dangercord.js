81abd713784362ae793a0edf01eff653b5077f8f
Philipp Herz <34943569+Pdzly@users.noreply.github.com>
2023-03-06 15:33:03 +0100

    does it work now?

--- build/models/Dangercord.js
+++
@@ -1,9 +0,0 @@
-export var DangercordReportCategories;
-(function (DangercordReportCategories) {
-    DangercordReportCategories[DangercordReportCategories["Other"] = 0] = "Other";
-    DangercordReportCategories[DangercordReportCategories["Advertising"] = 1] = "Advertising";
-    DangercordReportCategories[DangercordReportCategories["Spamming"] = 2] = "Spamming";
-    DangercordReportCategories[DangercordReportCategories["Raiding"] = 3] = "Raiding";
-    DangercordReportCategories[DangercordReportCategories["Harassing"] = 4] = "Harassing";
-})(DangercordReportCategories || (DangercordReportCategories = {}));
-//# sourceMappingURL=Dangercord.js.map
